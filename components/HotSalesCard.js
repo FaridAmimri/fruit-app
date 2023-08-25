0a018943b46df0c8577ec50b0f7d7ce447abef5a
Farid Amimri <farid.amimri@gmail.com>
2023-08-25 21:48:46 +0200

    Design Fruit & Hot Sales card

---
+++ components/HotSalesCard.js
@@ -0,0 +1,33 @@
+/** @format */
+
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+
+export default function HotSalesCard({ fruit }) {
+  return (
+    <View className='mr-6'>
+      <TouchableOpacity className='flex-row justify-center -mb-9 shadow-lg z-20'>
+        <Image
+          source={fruit.image}
+          style={{
+            height: 65,
+            width: 65,
+            shadowColor: fruit.shadow,
+            shadowRadius: 15,
+            shadowOffset: { width: 0, height: 20 },
+            shadowOpacity: 0.4,
+            overflow: 'visible'
+          }}
+        />
+      </TouchableOpacity>
+      <View
+        style={{ backgroundColor: fruit.color(0.4), height: 75, width: 80 }}
+        className='rounded-3xl flex items-center justify-end'
+      >
+        <Text className='font-semibold text-center text-gray-800 pb-3 tracking-wide'>
+          {fruit.price} €
+        </Text>
+      </View>
+    </View>
+  )
+}
