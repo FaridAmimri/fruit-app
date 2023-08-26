bb93e2a51c70652eb1857d6fa3c1e01af299c7e8
Farid Amimri <farid.amimri@gmail.com>
2023-08-26 13:06:45 +0200

    Add Product screen navigation

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -3,22 +3,29 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { LogBox } from 'react-native'
 import HomeScreen from '../screens/HomeScreen'
+import ProductScreen from '../screens/ProductScreen'
 
 const Stack = createNativeStackNavigator()
+
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state'
+])
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          contentStyle: { backgroundColor: 'white' }
-        }}
-      >
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen
           name='Home'
           options={{ headerShown: false }}
           component={HomeScreen}
+        />
+        <Stack.Screen
+          name='Product'
+          options={{ headerShown: false }}
+          component={ProductScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
